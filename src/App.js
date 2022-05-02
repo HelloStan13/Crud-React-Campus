60054bcf0cd9f310b38f197a166818379ec0f6a3
Adryan <adryanynfante@gmail.com>
2022-05-02 10:38:41 -0500

    Aplicacion localStorage
    cambios listos y local storage en proceso

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import UserTable from './tables/UserTable'
 const App = () => {
 	// Data
 	const usersData = [
-		{ id: 1, name: 'Tania', username: 'floppydiskette' },
-		{ id: 2, name: 'Craig', username: 'siliconeidolon' },
-		{ id: 3, name: 'Ben', username: 'benisphere' },
+		{ id: 1, name: 'Adryan', username: 'aynfante' }
 	]
 
 	const initialFormState = { id: null, name: '', username: '' }
